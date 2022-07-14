717a32173460b5e76cc23d186c38254ebd402904
Garuba Abdul Azeez <60158990+Azeezjim@users.noreply.github.com>
2022-07-14 17:13:24 +0000

    I edited the functions to carry out some specific tasks.

--- src/assignment.js
+++ src/assignment.js
@@ -4,35 +4,51 @@ const assignment = {};
 // ========================
 
 
-/**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
 
-/**
- * Challenge - 2
- * 
- * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
+ // * Challenge - 1
+ // * 
+ // * Calculate and return the sum of the numbers in an array.
+
+
+ // * If you did Challenge - 1, remove the comment in the line just after this function
+ // * 
+ // * @param {Array} arrayOfNumbers the array of numbers to sum
+ // * @returns number the sum of the numbers
+ 
+function sumOfNumbers(arrayOfNumbers) {
+  
+// const array = [1, 2, 3, 4];
+let sum = 0;
+
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
+}
+// console.log(sum);
+  return sum
+}
+assignment.sumOfNumbers = sumOfNumbers;
+
+
+ // * Challenge - 2
+ 
+ // * Count all even numbers within an array of numbers.
+ // * If you did Challenge - 2, remove the comment in the line just after this function
+ // * 
+ // * @param {Array} arrayOfNumbers the array containing even or non-even numbers
+ // * @returns number the count of even numbers
+ 
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // return arrayOfNumbers.length
+
+  const evenNumbers = arrayOfNumbers.filter(number => {
+  return number % 2 === 0
+})
+// console.log(even)
+  return evenNumbers.length
 }
 // assignment.countEvenNumbers = countEvenNumbers;
 
-/**
+/*
  * Challenge - 3
  * 
  * Given an array of numbers representing temperatures in Celsius,
